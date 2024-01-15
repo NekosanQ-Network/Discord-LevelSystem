e2be834f59991bfef1fd6d5b8388124a6fabd9f6
NekosanQ <NekosanQ@outlook.com>
2024-01-15 19:37:42 +0900

    ボーナスプログラムの追加

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -12,7 +12,7 @@ module.exports = {
 
         const now = Date.now();
         const lastMessage = coolDownMap.get(message.author.id);
-        const bonus = bonusMap.get(message.author.id);
+        const bonus: number| undefined = bonusMap.get(message.author.id);
 
         if (lastMessage) {
             console.log(`[クールダウンテスト] user_id=${message.author.id}, クールダウン中`);
@@ -22,19 +22,18 @@ module.exports = {
         if (!bonus) {
             console.log(`[ボーナステスト♡] user_id=${message.author.id} 作成したよ♡`);
             bonusMap.set(message.author.id, 1);
-        }
+        } else {
+            const xp: number = Math.floor(Math.random() * (20 - 1 + 1)) +  1;
+            if (bonus <= 10) {
+                console.log(`取得した経験値: ${xp}`);
+            };
+            
+            coolDownMap.set(message.author.id, now); // ここで登録
 
-        const xp: number = Math.floor(Math.random() * (20 - 1 + 1)) +  1;
-        if (bonus <= 10)
-        {
-            console.log(`取得した経験値: ${xp}`);
+            setTimeout(() => {
+                coolDownMap.delete(message.author.id);
+                console.log(`[クールダウンテスト] user_id=${message.author.id} 解除`)
+            }, 5000); // 5秒待機して、解除
         }
-        
-        coolDownMap.set(message.author.id, now); // ここで登録
-
-        setTimeout(() => {
-            coolDownMap.delete(message.author.id);
-            console.log(`[クールダウンテスト] user_id=${message.author.id} 解除`)
-        }, 5000); // 5秒待機して、解除
     }
 };
