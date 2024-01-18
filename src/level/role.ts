f8b25429ac4c2688214ecc8a05ce33ecc115de1c
べーこんにみそ <Kinosoft@outlook.jp>
2024-01-19 01:12:06 +0900

    コメントの追加

--- src/level/role.ts
+++ src/level/role.ts
@@ -8,15 +8,15 @@ export async function grantRole(message: Message, xp: number) : Promise<void> {
     const member = await message.guild?.members.fetch(message.author.id);
 
     try {
-        if (xp >= 1000000 && !member?.roles.cache.has(config.roleIds[4])) {
+        if (xp >= 1000000 && !member?.roles.cache.has(config.roleIds[4])) { // "猫神"
             member?.roles.add(config.roleIds[4]);
-        } else if (xp >= 500000 && !member?.roles.cache.has(config.roleIds[3])) {
+        } else if (xp >= 500000 && !member?.roles.cache.has(config.roleIds[3])) { // "達人"
             member?.roles.add(config.roleIds[3]);
-        } else if (xp >= 200000 && !member?.roles.cache.has(config.roleIds[2])) {
+        } else if (xp >= 200000 && !member?.roles.cache.has(config.roleIds[2])) { // "常連"
             member?.roles.add(config.roleIds[2]);
-        } else if (xp >= 50000 && !member?.roles.cache.has(config.roleIds[1])) {
+        } else if (xp >= 50000 && !member?.roles.cache.has(config.roleIds[1])) { // "新規卒"
             member?.roles.add(config.roleIds[1]);
-        } else if (xp >= 100 && !member?.roles.cache.has(config.roleIds[0])) {
+        } else if (xp >= 100 && !member?.roles.cache.has(config.roleIds[0])) { // "新規"
             member?.roles.add(config.roleIds[0]);
         };
     } catch (error) {
