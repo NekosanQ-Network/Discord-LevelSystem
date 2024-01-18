e4f356416881f32a62005cdcd118694326cae01e
べーこんにみそ <Kinosoft@outlook.jp>
2024-01-19 01:23:26 +0900

    役職付与の処理追加。 TODO追加。 テスト用の経験値データ(Map形式)を追加

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,7 +1,20 @@
 import { Events, Message } from 'discord.js';
+import { grantRole } from '../level/role.js';
 
 const coolDownMap = new Map<string, number>();
 const bonusMap = new Map<string, number>();
+
+// テストコードです。 経験値を格納するのに使用します。
+const users = new Map<string, number>();
+
+/* TODO: 経験値獲得処理の部分を、もうちょっといい感じにする。
+    -> 例えば！: データ新規作成でも、きちんと、経験値を獲得できるようにする。
+
+    -> (完成次第、)データベースに書き込めるようにすることを忘れないように。
+
+    2024.01.19 AM1時
+*/
+
 // -----------------------------------------------------------------------------------------------------------
 // メッセージ処理
 // -----------------------------------------------------------------------------------------------------------
@@ -19,24 +32,38 @@ module.exports = {
             return;
         };
 
-        const xp: number = Math.floor(Math.random() * (20 - 1 + 1)) +  1;
-        if (bonusCounter) {
-            bonusMap.set(message.author.id, bonusCounter + 1);
-            if (bonusCounter < 10) {
-                console.log(`取得した経験値: ${xp * 5}`);
-                console.log(`[ボーナステスト] user_id=${message.author.id} 回数更新, 現在: ${bonusMap.get(message.author.id)}`);
+        const obtainXp: number = Math.floor(Math.random() * (20 - 1 + 1)) +  1; // 獲得XP
+        let xp: number | undefined = users.get(message.author.id);  // <-- テストコード。 自分が持っている経験値
+        if (!xp) { // <-- データがない場合は新規作成。(もうちょっといい処理があるんだけど、思いつかないので一旦放置。)
+            users.set(message.author.id, 1);
+        }
+        else {
+            if (bonusCounter) {
+                bonusMap.set(message.author.id, bonusCounter + 1);
+                if (bonusCounter < 10) {
+                    xp += obtainXp * 5;
+
+                    console.log(`取得した経験値: ${obtainXp * 5}`);
+                    console.log(`[ボーナステスト] user_id=${message.author.id} 回数更新, 現在: ${bonusMap.get(message.author.id)}`);
+                } else {
+                    console.log(`取得した経験値: ${obtainXp}`);
+                    xp += obtainXp;
+                };
+
             } else {
-                console.log(`取得した経験値: ${xp}`);
+                bonusMap.set(message.author.id, 1); // ボーナスカウンタ 新規作成
+                console.log(`[ボーナステスト] user_id=${message.author.id} 作成したよ`);
+
+                console.log(`取得した経験値: ${obtainXp * 5}`);
+                console.log(`[ボーナステスト] user_id=${message.author.id} 回数更新, 現在: ${bonusMap.get(message.author.id)}`);
+                xp += obtainXp * 5;
             };
 
-        } else {
-            bonusMap.set(message.author.id, 1); // ボーナスカウンタ 新規作成
-            console.log(`[ボーナステスト] user_id=${message.author.id} 作成したよ`);
-
-            console.log(`取得した経験値: ${xp * 5}`);
-            console.log(`[ボーナステスト] user_id=${message.author.id} 回数更新, 現在: ${bonusMap.get(message.author.id)}`);
+            console.log(`${message.author.id} の現在持っているXP: ${xp}`);
+            users.set(message.author.id, xp);
+            grantRole(message, xp);
         };
-    
+
         coolDownMap.set(message.author.id, now); // ここで登録
 
         setTimeout(() => {
