82c4ab9a8af201ef14403c6cc2d6ecaa557f3507
べーこんにみそ <Kinosoft@outlook.jp>
2024-01-19 01:09:13 +0900

    特定の経験値に達したときの処理。 新規作成です。

---
+++ src/level/role.ts
@@ -0,0 +1,25 @@
+import { Message } from "discord.js";
+import { config } from "../utils/config";
+
+// -----------------------------------------------------------------------------------------------------------
+// 特定XPに達したときに、それに応じた役職を付与する
+// -----------------------------------------------------------------------------------------------------------
+export async function grantRole(message: Message, xp: number) : Promise<void> {
+    const member = await message.guild?.members.fetch(message.author.id);
+
+    try {
+        if (xp >= 1000000 && !member?.roles.cache.has(config.roleIds[4])) {
+            member?.roles.add(config.roleIds[4]);
+        } else if (xp >= 500000 && !member?.roles.cache.has(config.roleIds[3])) {
+            member?.roles.add(config.roleIds[3]);
+        } else if (xp >= 200000 && !member?.roles.cache.has(config.roleIds[2])) {
+            member?.roles.add(config.roleIds[2]);
+        } else if (xp >= 50000 && !member?.roles.cache.has(config.roleIds[1])) {
+            member?.roles.add(config.roleIds[1]);
+        } else if (xp >= 100 && !member?.roles.cache.has(config.roleIds[0])) {
+            member?.roles.add(config.roleIds[0]);
+        };
+    } catch (error) {
+        console.error(`[役職付与] ${error}`);
+    }
+}
