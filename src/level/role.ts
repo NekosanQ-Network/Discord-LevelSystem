9bc6706ea9c86643ab099c12c1b2c3f92b9a264b
べーこんにみそ <Kinosoft@outlook.jp>
2024-01-21 15:57:54 +0900

    Botより高い位置にいるユーザーに関しての処理を行わないようにした。/  基準を下回ったときに役職を剥奪する処理を追加した。

--- src/level/role.ts
+++ src/level/role.ts
@@ -6,20 +6,42 @@ import { config } from "../utils/config";
 // -----------------------------------------------------------------------------------------------------------
 export async function grantRole(message: Message, xp: number) : Promise<void> {
     const member = await message.guild?.members.fetch(message.author.id);
+    const bot = await message.guild?.members.fetch(config.clientId);
+    if (member?.roles.highest.position! > bot?.roles.highest.position!) { // NOTE: Botより高いやつにはあげない
+        console.log(`あげようとしたやつ、Botより高いステージにいるから、役職あげないよ。帰れ。`);
+        return;
+    }
 
-    try {
-        if (xp >= 1000000 && !member?.roles.cache.has(config.roleIds[4])) { // "猫神"
-            member?.roles.add(config.roleIds[4]);
-        } else if (xp >= 500000 && !member?.roles.cache.has(config.roleIds[3])) { // "達人"
-            member?.roles.add(config.roleIds[3]);
-        } else if (xp >= 200000 && !member?.roles.cache.has(config.roleIds[2])) { // "常連"
-            member?.roles.add(config.roleIds[2]);
-        } else if (xp >= 50000 && !member?.roles.cache.has(config.roleIds[1])) { // "新規卒"
-            member?.roles.add(config.roleIds[1]);
-        } else if (xp >= 100 && !member?.roles.cache.has(config.roleIds[0])) { // "新規"
-            member?.roles.add(config.roleIds[0]);
-        };
-    } catch (error) {
-        console.error(`[役職付与] ${error}`);
+    if (xp >= 1000000 && !member?.roles.cache.has(config.roleIds[4])) { // "猫神"
+        member?.roles.add(config.roleIds[4]);
+    } else if (xp >= 500000 && !member?.roles.cache.has(config.roleIds[3])) { // "達人"
+        member?.roles.add(config.roleIds[3]);
+    } else if (xp >= 200000 && !member?.roles.cache.has(config.roleIds[2])) { // "常連"
+        member?.roles.add(config.roleIds[2]);
+    } else if (xp >= 50000 && !member?.roles.cache.has(config.roleIds[1])) { // "新規卒"
+        member?.roles.add(config.roleIds[1]);
+    } else if (xp >= 100 && !member?.roles.cache.has(config.roleIds[0])) { // "新規"
+        member?.roles.add(config.roleIds[0]);
+    };
+}
+// -----------------------------------------------------------------------------------------------------------
+// 基準を下回ったときに、役職を剥奪する
+// -----------------------------------------------------------------------------------------------------------
+export async function deprivationRole(message: Message, xp: number) : Promise<void> {
+    const member = await message.guild?.members.fetch(message.author.id);
+    const bot = await message.guild?.members.fetch(config.clientId);
+    if (member?.roles.highest.position! > bot?.roles.highest.position!) { // NOTE: Botより高いやつからは奪わない
+        console.log(`奪おうとしたやつ、Botより高いステージにいるから、役職奪えないよ。帰れ。`);
+        return;
+    }
+
+    if (xp < 1000000 && member?.roles.cache.has(config.roleIds[4])) {
+        member?.roles.remove(config.roleIds[4]);
+    } else if (xp < 500000 && member?.roles.cache.has(config.roleIds[3])) {
+        member?.roles.remove(config.roleIds[3]);
+    } else if (xp < 200000 && member?.roles.cache.has(config.roleIds[2])) {
+        member?.roles.remove(config.roleIds[2]);
+    } else if (xp < 50000 && member?.roles.cache.has(config.roleIds[1])) {
+        member?.roles.remove(config.roleIds[1]);
     }
 }
