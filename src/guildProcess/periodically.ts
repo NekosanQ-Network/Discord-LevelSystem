88737a888fdbb13f10afbb54b408f0e1bb560fc9
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-19 02:32:39 +0900

    実行を 毎週土曜日午前0時にするようにした

--- src/guildProcess/periodically.ts
+++ src/guildProcess/periodically.ts
@@ -3,7 +3,7 @@ import cron from "node-cron";
 import { periodicExecution } from "../module/file/periodicExecution"
 module.exports = {
     async execute(client: Client): Promise<void> {
-        cron.schedule("0 0 0 * * *", async () => {
+        cron.schedule("0 0 * * 1", async () => { // 毎週土曜日 午前0時0分
             await periodicExecution(client);
         });
     }
