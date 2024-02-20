6fac11a839944fba783a215a16730f3ccfbfb43c
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-21 00:50:38 +0900

    VCのボーナス不具合修正

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -1,13 +1,16 @@
-import { Events, VoiceState } from "discord.js";
+import { Events, Guild, VoiceState } from "discord.js";
 import { vcBonusMap, earnedXpMap } from "../module/file/periodicExecution";
 import { PrismaClient } from "@prisma/client";
+import { sendDataToProcessId } from "pm2";
 const prisma = new PrismaClient();
 
 /**
+ *  ボイスチャンネル滞在
+ * 
     @type {string} ユーザーId
     @type {number} 接続した時間(UNIX TIME)
 */
-const vcConnectTimeMap = new Map<string, number>();
+export const vcConnectTimeMap = new Map<string, number>();
 
 module.exports = {
     name: Events.VoiceStateUpdate,
@@ -33,19 +36,23 @@ module.exports = {
                 vcConnectTimeMap.set(userId, unixTimeStamp);
                 console.log(`[VC接続] user_id: ${userId}, unixTimeStamp(JoinedTime): ${unixTimeStamp}`);
             } else if (oldState.channel != null && newState.channel === null && vcConnectTime) {
-                if (!vcBonusMap.get(userId) && unixTimeStamp - vcConnectTime >= 600) {
+                if (!vcBonusMap.get(userId)) {
                     xp = grantXP(userId, vcConnectTime, unixTimeStamp, allUsers[0].user_xp, true);
                 } else {
-                    xp = grantXP(userId, vcConnectTime, unixTimeStamp, allUsers[0].user_xp, false);
-                };
+                    xp = grantXP(userId, vcConnectTime, unixTimeStamp, allUsers[0].user_xp, vcBonusMap.get(userId)! >= 600 ? false : true);
+                }
+
+                vcConnectTimeMap.delete(userId); // 出たら削除
 
             } else if (oldState.channel != null && newState.channel != null && vcConnectTime) {
-                if (!vcBonusMap.get(userId) && unixTimeStamp - vcConnectTime >= 600) {
+                if (!vcBonusMap.get(userId)) {
                     xp = grantXP(userId, vcConnectTime, unixTimeStamp, allUsers[0].user_xp, true);
                 } else {
-                    xp = grantXP(userId, vcConnectTime, unixTimeStamp, allUsers[0].user_xp, false);
-                };
-            };
+                    xp = grantXP(userId, vcConnectTime, unixTimeStamp, allUsers[0].user_xp, vcBonusMap.get(userId)! >= 600 ? false : true);
+                }
+
+                vcConnectTimeMap.delete(userId); // 出たら削除
+            }
 
             if (allUsers) {
                 await prisma.levels.updateMany({
@@ -55,14 +62,14 @@ module.exports = {
                     data: {
                         user_xp: allUsers[0].user_xp + xp
                     }
-                });
+                })
             } else {
                 await prisma.levels.create({
                     data: {
                         user_id: userId,
                         user_xp: xp
                     }
-                });
+                })
             }
         } catch (error) {
             console.log(error);
@@ -77,19 +84,27 @@ module.exports = {
  * @param xp 実行ユーザー経験値
  * @param isBonus ボーナス付与するか？
  */
-function grantXP(userId: string, joinedTime: number, leftTime: number, xp: number, isBonus: boolean) : number {
-    let earnExp: number = Math.floor((leftTime - joinedTime) / 10);
-    earnExp -= isBonus ? 60 : 0;                    // ボーナス有効時 -60
-    let bonusExp: number = isBonus ? 60 * 2 : 0;    // ボーナス有効時 60 * 2
+export function grantXP(userId: string, joinedTime: number, leftTime: number, xp: number, isBonus: boolean) : number {
+    let connectionTime = leftTime - joinedTime;
+    let earnExp: number = 0;
 
-    if (isBonus) {
-        vcBonusMap.set(userId, 1); // ボーナスを受け取れないようにする
-    };
+    let vcBonus: number | undefined = vcBonusMap.get(userId);
+    if (!vcBonus) {
+        vcBonusMap.set(userId, connectionTime >= 600 ? 600 : connectionTime);
+    } else {
+        vcBonusMap.set(userId, connectionTime >= 600 ? 600 : vcBonus + connectionTime);
+    }
+
+    vcBonus = vcBonusMap.get(userId);
+    if (isBonus)
+        earnExp = Math.floor(connectionTime / 10) <= 60 ? Math.floor(connectionTime / 10) * 2 : (60 - Math.floor(connectionTime / 10)) * -1 + 60 * 2;
+    else
+        earnExp = Math.floor(connectionTime / 10);
 
     const earnedEXP : number | undefined = earnedXpMap.get(userId); // その日稼いだ経験値
-    earnedXpMap.set(userId, (earnedEXP ? earnedEXP : 0) + earnExp + bonusExp);
+    earnedXpMap.set(userId, (earnedEXP ? earnedEXP : 0) + earnExp);
 
-    console.log(`user_id: ${userId}, 獲得XP(計): ${earnExp + bonusExp}, 内訳(ボーナス対象外): ${earnExp}, 内訳(ボーナス対象): ${bonusExp}`);
+    console.log(`user_id: ${userId}, 獲得XP: ${earnExp}, 合計接続時間(秒): ${vcBonus}, 合計XP: ${earnedXpMap.get(userId)}`);
 
-    return earnExp + bonusExp;
+    return earnExp;
 };
