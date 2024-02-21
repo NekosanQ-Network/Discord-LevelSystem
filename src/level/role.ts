0e25fa60646af5dbcf9960c3ba6bbff71bb75baa
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-21 20:13:25 +0900

    GrantRoleメソッドのパラメーターを変更。

--- src/level/role.ts
+++ src/level/role.ts
@@ -8,27 +8,27 @@ import { config } from "../utils/config";
 const levels: number[] = [1000000, 500000, 200000, 50000, 100];
 
 /**
- * 実行ユーザーの経験値が一定基準を満たしたとき、それに応じた役職を付与する
- * @param message メッセージデータ
- * @param xp 実行ユーザーの経験値
+ * 役職を付与する
+ * @param userId 対象ユーザー
+ * @param guild 実行Guild
+ * @param xp 現在の経験値
  * @returns なし
  */
-export async function grantRole(message: Message, xp: number) : Promise<void> {
-    const member = await message.guild?.members.fetch(message.author.id);
-    const bot = await message.guild?.members.fetch(config.clientId);
-    if (member?.roles.highest.position! > bot?.roles.highest.position!) { // NOTE: Botより高いやつにはあげない
-        console.log(`user_id: ${message.author.id} -> 役職付与できない。`);
-        return;
-    };
+export async function grantRole(userId: string, guild: Guild, xp: number) : Promise<void> {
+    const member  = await guild.members.fetch(userId);
+    const bot = await guild.members.fetch(config.clientId);
 
-    const roles : string[] = config.roleIds.slice(0, 5).reverse();
+    if (member?.roles.highest.position! > bot?.roles.highest.position!) // Botより(役職位置が)高い人には付与しない
+        return;
+    
+    const roles: string[] = config.roleIds.slice(0, 5).reverse();
     for (let i = 0; i < levels.length; i++) {
         if (xp >= levels[i] && !member?.roles.cache.has(config.roleIds[i])) {
             member?.roles.add(roles[i]);
             break;
-        };
-    };
-};
+        }
+    }
+}
 
 /**
  * 実行ユーザーの経験値が一定基準より下回ったとき、役職を剥奪する
