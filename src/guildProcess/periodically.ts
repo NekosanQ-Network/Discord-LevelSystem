dda30bfeafcdeaac4eb24087435fe83eaa9cf49b
べーこんにみそ <Kinosoft@outlook.jp>
2024-02-23 15:56:53 +0900

    スケジューラー。 土曜日実行。

--- src/guildProcess/periodically.ts
+++ src/guildProcess/periodically.ts
@@ -1,10 +1,9 @@
 import { Client } from "discord.js";
 import cron from "node-cron";
 import { periodicExecution } from "../module/file/periodicExecution"
-module.exports = {
-    async execute(client: Client): Promise<void> {
-        cron.schedule("0 0 * * 1", async () => { // 毎週土曜日 午前0時0分
-            await periodicExecution(client);
-        });
-    }
-};
+
+export async function executePeriodically(client: Client): Promise<void> {
+    cron.schedule("0 0 * * 6", async () => { // 毎週土曜日 午前0時0分 (0 0 * * 6)
+        await periodicExecution(client);
+    });
+}
